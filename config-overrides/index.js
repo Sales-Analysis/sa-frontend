f642b033b0669d9910dfe7a2790021a843716ba4
Kirill Sadovnikov <d4rkm3z@gmail.com>
2021-07-26 23:49:14 +0300

    feat(Configuration): setup postcss

---
+++ config-overrides/index.js
@@ -0,0 +1,10 @@
+module.exports = config => {
+  config.module.rules.push(
+    {
+      test: /\.css$/i,
+      use: ["postcss-loader"],
+    },
+  );
+
+  return config;
+};
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
-const baseConfig = require('@sales-analysis/sa-frontend-configs/postcss.config');
-
 module.exports = {
   plugins: [
-    ...baseConfig().plugins,
     require('postcss-nested'),
     require('autoprefixer')
   ]
