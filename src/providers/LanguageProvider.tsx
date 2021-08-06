d6793b54098dc23b8473845c46ef25c63460b85f
Kirill Sadovnikov <d4rkm3z@gmail.com>
2021-08-06 20:09:29 +0100

    feat(providers): implementing i18n and custom theme

--- src/providers/LanguageProvider.tsx
+++ src/providers/LanguageProvider.tsx
@@ -3,7 +3,9 @@ import { IntlProvider } from 'react-intl';
 import { Loader } from '@consta/uikit/Loader';
 import { MessageFormatElement } from '@formatjs/icu-messageformat-parser';
 
-async function loadLocaleData(locale: string): Promise<Record<string, MessageFormatElement[]>> {
+async function loadLocaleData(
+  locale: string
+): Promise<Record<string, MessageFormatElement[]>> {
   switch (locale) {
     default:
       // TODO: fix it in the feature by lang changing implemented
@@ -13,25 +15,29 @@ async function loadLocaleData(locale: string): Promise<Record<string, MessageFor
   }
 }
 
-export const LanguageProvider = React.memo<PropsWithChildren<ReactNode>>(function LocaleProvider({children}) {
-  const locale = navigator.language;
-  const [messages, setMessages] = useState<Record<string, MessageFormatElement[]> | null>(null);
+export const LanguageProvider = React.memo<PropsWithChildren<ReactNode>>(
+  function LocaleProvider({ children }) {
+    const locale = navigator.language;
+    const [messages, setMessages] = useState<Record<
+      string,
+      MessageFormatElement[]
+    > | null>(null);
 
-  useEffect(() => {
-    async function loadTranslations() {
-      const translation = await loadLocaleData(locale);
-      setMessages(translation);
-    }
+    useEffect(() => {
+      async function loadTranslations() {
+        const translation = await loadLocaleData(locale);
+        setMessages(translation);
+      }
 
-    loadTranslations().finally();
-  }, [locale]);
+      loadTranslations().finally();
+    }, [locale]);
 
-  return (messages ?
-      <IntlProvider
-        locale={locale}
-        defaultLocale="ru-RU"
-      >
+    return messages ? (
+      <IntlProvider locale={locale} defaultLocale="ru-RU">
         {children}
-      </IntlProvider> : <Loader />
-  );
-});
+      </IntlProvider>
+    ) : (
+      <Loader />
+    );
+  }
+);
--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,15 +1,18 @@
 import React, { PropsWithChildren, ReactNode } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { presetGpnDefault, Theme } from '@consta/uikit/Theme';
+import { ApolloProvider } from '@apollo/client';
+import { Theme } from '@consta/uikit/Theme';
+import { client } from 'apollo';
+import { presetDefault } from 'theme/presets/presetDefault';
 import { LanguageProvider } from './LanguageProvider';
 
-export const Providers: React.FC<PropsWithChildren<ReactNode>> = ({children}) => {
+export const Providers: React.FC<PropsWithChildren<ReactNode>> = ({ children }) => {
   return (
     <LanguageProvider>
-      <Theme preset={presetGpnDefault}>
-        <Router>
-          {children}
-        </Router>
+      <Theme preset={presetDefault}>
+        <ApolloProvider client={client}>
+          <Router>{children}</Router>
+        </ApolloProvider>
       </Theme>
     </LanguageProvider>
   );
